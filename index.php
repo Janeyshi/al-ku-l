dbf358ea56a84623e7508bc90b4d04e410a848e6
JC-Dulos10 <[201801028@iacademy.edu.ph]>
2020-05-10 16:58:32 +0800

    Added Base Layout

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+  require_once('Finals/view/header.php');
+ ?>
+    <link rel="stylesheet" type="text/css" href="Finals/CSS/style.css">
+    <h3 class="card-title">Home Page</h3>
+    <form action="process-order.php" method="post">
+      <table class="table">
+        <thead>
+          <h1 class="center-text"> Hello World </h1>
+        </thead>
+        <tbody>
+          <!-- Content -->
+
+
+        </tbody>
+      </table>
+    </form>
+  <?php require_once('Finals/view/footer.php'); ?>
