173f7825685a4adc20faabfeb052bd00644d6d57
JC-Dulos10 <[201801028@iacademy.edu.ph]>
2020-05-15 19:37:05 +0800

    Added Home page

--- Finals/index.php
+++ Finals/index.php
@@ -49,7 +49,7 @@
 
               <div class="row">
                 <div class="col-lg-4 col-md-6">
-                  <div class="speaker">
+                  <div>
                     <img src="images/pic1.jpg" alt="pic ni jc" class="img-fluid" height="250px" width="250px">
                     <div class="details">
                       <h3>Juan Carlos C. Dulos</h3>
@@ -58,7 +58,7 @@
                   </div>
                 </div>
                 <div class="col-lg-4 col-md-6">
-                  <div class="speaker">
+                  <div>
                     <img src="images/pic2.jpg" alt="pic ni liam" class="img-fluid" height="250px" width="250px">
                     <div class="details">
                       <h3>James Liam De jesus</h3>
@@ -67,7 +67,7 @@
                   </div>
                 </div>
                 <div class="col-lg-4 col-md-6">
-                  <div class="speaker">
+                  <div>
                     <img src="images/pic3.jpg" alt="pic ni denise" class="img-fluid" height="250px" width="250px">
                     <div class="details">
                       <h3>Kaithelyn Jose</h3>
@@ -76,7 +76,7 @@
                   </div>
                 </div>
                 <div class="col-lg-4 col-md-6">
-                  <div class="speaker">
+                  <div>
                     <img src="images/pic2.jpg" alt="pic ni janey" class="img-fluid" height="250px" width="250px">
                     <div class="details">
                       <h3>Janedel Ac-ac</h3>
@@ -85,11 +85,20 @@
                   </div>
                 </div>
                 <div class="col-lg-4 col-md-6">
-                  <div class="speaker">
+                  <div>
                     <img src="images/pic1.jpg" alt="pic ni ben" class="img-fluid" height="250px" width="250px">
                     <div class="details">
                       <h3>Benedick Macayan</h3>
                       <p>Researcher</p>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-lg-4 col-md-6">
+                  <div>
+                    <img src="images/team.png" alt="pic ni ben" class="img-fluid" height="250px" width="250px">
+                    <div class="details">
+                      <h3>Everyone</h3>
+                      <p>Regradless of the assigned job everyone did their part in the code as well</p>
                     </div>
                   </div>
                 </div>
